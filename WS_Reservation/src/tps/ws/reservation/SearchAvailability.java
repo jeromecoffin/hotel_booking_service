b457b0daf754202d0f2a8c0f10ce224359ef1584
jeromecoffin <jeroome.coffin@efrei.net>
2020-11-23 10:50:23 +0100

    client request

--- WS_Reservation/src/tps/ws/reservation/SearchAvailability.java
+++ WS_Reservation/src/tps/ws/reservation/SearchAvailability.java
@@ -5,17 +5,23 @@ import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+import com.sun.xml.internal.ws.api.message.Message;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchAvailability extends ServerResource {
+	
+	private List<String> messages = new ArrayList<String>();
 
 	@Get
-	public void main(Representation entity) {
+	public List<String> main(Representation entity) {
 		
 		String date = (String) getRequestAttributes().get("date");
 		String nights = (String) getRequestAttributes().get("nights");
@@ -43,10 +49,15 @@ public class SearchAvailability extends ServerResource {
 	        String output;
 	        System.out.println("Output from Server .... \n");
 	        while ((output = br.readLine()) != null) {
-	            System.out.println(output);
+	            //System.out.println(output);
+	            messages.add(output);
 	        }
 
 	        conn.disconnect();
+	        
+	        //for(int i = 0; i < messages.size(); i++) {
+	        //    System.out.println(messages.get(i));
+	        //}
 
 	      } catch (MalformedURLException e) {
 
@@ -57,6 +68,7 @@ public class SearchAvailability extends ServerResource {
 	        e.printStackTrace();
 
 	      }
+		return messages;
 
 	    }
 }  
