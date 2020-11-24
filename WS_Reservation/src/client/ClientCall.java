636abb6adf891d1bb5958707d815a835930413db
jeromecoffin <jeroome.coffin@efrei.net>
2020-11-24 09:50:08 +0100

    nettoyage

--- WS_Reservation/src/client/ClientCall.java
+++ WS_Reservation/src/client/ClientCall.java
@@ -20,38 +20,39 @@ public class ClientCall {
 		int id=3;
 		
 		try {
-		URL urlSearch = new URL("http://localhost:8080/WS_Reservation/search/"+date+"/"+nights+"/"+rooms);
-	    HttpURLConnection connSearch = (HttpURLConnection) urlSearch.openConnection();
-	    connSearch.setRequestMethod("GET");
-	    connSearch.setRequestProperty("Accept", "application/json");
-	    
-	    if (connSearch.getResponseCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + connSearch.getResponseCode());
-        }
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-            (connSearch.getInputStream())));
-
-        String output;
-        System.out.println("Output from Server .... \n");
-        while ((output = br.readLine()) != null) {
-            System.out.println(output);
-        }
-        
-        connSearch.disconnect();
-        
-        //TODO: proposer une liste et mettre a jour les variables
-        
-        ClientResource resource = new ClientResource("http://localhost:8080/WS_Reservation/reservation");  
-        
-		Form form = new Form();  
-		form.add("id", Integer.toString(id));  
-		form.add("date", date);
-		form.add("nights", Integer.toString(nights));
-		form.add("rooms", Integer.toString(rooms));
- 
-		resource.post(form).write(System.out);
+			
+			URL urlSearch = new URL("http://localhost:8080/WS_Reservation/search/"+date+"/"+nights+"/"+rooms);
+		    HttpURLConnection connSearch = (HttpURLConnection) urlSearch.openConnection();
+		    connSearch.setRequestMethod("GET");
+		    connSearch.setRequestProperty("Accept", "application/json");
+		    
+		    if (connSearch.getResponseCode() != 200) {
+	            throw new RuntimeException("Failed : HTTP error code : "
+	                    + connSearch.getResponseCode());
+	        }
+	
+	        BufferedReader br = new BufferedReader(new InputStreamReader(
+	            (connSearch.getInputStream())));
+	
+	        String output;
+	        System.out.println("Output from Server .... \n");
+	        while ((output = br.readLine()) != null) {
+	            System.out.println(output);
+	        }
+	        
+	        connSearch.disconnect();
+	        
+	        //TODO: proposer une liste et mettre a jour les variables
+	        
+	        ClientResource resource = new ClientResource("http://localhost:8080/WS_Reservation/reservation");  
+	        
+			Form form = new Form();  
+			form.add("id", Integer.toString(id));  
+			form.add("date", date);
+			form.add("nights", Integer.toString(nights));
+			form.add("rooms", Integer.toString(rooms));
+	 
+			resource.post(form).write(System.out);
  
 	    
 		} catch (MalformedURLException e) {
